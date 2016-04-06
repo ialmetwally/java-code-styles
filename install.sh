a61f083eb64d8e9cdf5145b0c9417be99982975b
Ismail Almetwally <ialmetwally@users.noreply.github.com>
2016-04-06 13:48:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,4 +26,4 @@ done
 
 echo "Done."
 echo ""
-echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Square' or 'SquareAndroid'."
+echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Lamudi' or 'LamudiAndroid'."
